5f92433bdda1c691a5e884c793f301af3cfea14c
monree <monreestudio@outlook.com>
2020-10-19 17:47:22 +0800

    好像又解决了一些问题。

--- app/src/main/jni/Test.cpp
+++ app/src/main/jni/Test.cpp
@@ -8,27 +8,6 @@
 #include <setjmp.h>
 #include <pthread.h>
 
-int checkExc(JNIEnv *env) {
-    if(env->ExceptionCheck()) {
-        env->ExceptionDescribe(); // writes to logcat
-        env->ExceptionClear();
-        return 1;
-    }
-    return -1;
-}
-
-void JNU_ThrowByName(JNIEnv *env, const char *name, const char *msg)
-{
-    // 查找异常类
-    jclass cls = env->FindClass(name);
-    /* 如果这个异常类没有找到，VM会抛出一个NowClassDefFoundError异常 */
-    if (cls != NULL) {
-        env->ThrowNew(cls, msg);  // 抛出指定名字的异常
-    }
-    /* 释放局部引用 */
-    env->DeleteLocalRef(cls);
-}
-
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_monree_crashcatch_JNI_test(
@@ -41,30 +20,21 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_monree_crashcatch_JNI_test2(JNIEnv *env, jclass clazz) {
     jthrowable exc = NULL;
-//    int *p = 0; //空指针
-//    *p = 1;
-    jmethodID mid = env->GetStaticMethodID(clazz,"exceptionCallback","()V");
-    if (mid != NULL) {
-        env->CallStaticVoidMethod(clazz,mid);
-    }
-//    if (env->ExceptionCheck()) {  // 检查JNI调用是否有引发异常
-//        env->ExceptionDescribe();
-//        jthrowable jthrowable = env->ExceptionOccurred();
-//        env->ExceptionClear();        // 清除引发的异常，在Java层不会打印异常的堆栈信息
-//        env->Throw(jthrowable);
-//        //env->ThrowNew(env->FindClass("java/lang/Exception"), "哦豁");
-//        return;
+    int *p = 0; //空指针
+    *p = 1;
+//    jmethodID mid = env->GetStaticMethodID(clazz,"exceptionCallback","()V");
+//    if (mid != NULL) {
+//        env->CallStaticVoidMethod(clazz,mid);
 //    }
-    mid = env->GetStaticMethodID(clazz,"normalCallback","()V");
-    if (mid != NULL) {
-        env->CallStaticVoidMethod(clazz,mid);
-    }
+//    mid = env->GetStaticMethodID(clazz,"normalCallback","()V");
+//    if (mid != NULL) {
+//        env->CallStaticVoidMethod(clazz,mid);
+//    }
 }
 
 // 定义代码跳转锚点
 sigjmp_buf JUMP_ANCHOR;
 volatile sig_atomic_t error_cnt = 0;
-JNIEnv *mEnv;
 JavaVM *jvm;
 
 void exception_handler(int errorCode){
@@ -73,19 +43,35 @@ void exception_handler(int errorCode){
     assert (rs == JNI_OK);
 
     error_cnt += 1;
-    LOGE("JNI_ERROR, error code %d, cnt %d", errorCode, error_cnt);
+//    LOGE("JNI_ERROR, error code %d, cnt %d", errorCode, error_cnt);
 
     // DO SOME CLEAN STAFF HERE...
     if (mEnv && mEnv->ExceptionCheck()) {  // 检查JNI调用是否有引发异常
-        LOGE("杭杭杭杭杭杭和", errorCode, error_cnt);
+        LOGE("检测到异常", errorCode, error_cnt);
         mEnv->ExceptionDescribe();
+//        LOGE("上面为异常描述", errorCode, error_cnt);
         jthrowable jthrowable = mEnv->ExceptionOccurred();
+        if(jthrowable == nullptr)
+            LOGE("异常为空", errorCode, error_cnt);
         mEnv->ExceptionClear();        // 清除引发的异常，在Java层不会打印异常的堆栈信息
-//        mEnv->Throw(jthrowable);
-        mEnv->ThrowNew(mEnv->FindClass("java/lang/Exception"), "哦豁");
+        mEnv->Throw(jthrowable);
+        LOGE("向外抛出异常", errorCode, error_cnt);
+//        jvm->DetachCurrentThread();
+        // jump to main function to do exception process
+//        mEnv->ThrowNew(mEnv->FindClass("java/lang/Exception"), "哦豁");
+        siglongjmp(JUMP_ANCHOR, 0);
         return;
     } else {
-        LOGE("??????", 0, 0);
+            if(mEnv == nullptr) {
+                LOGE("mEnv为空哦", 0, 0);
+            }
+            else {
+                LOGE("莫得办法了", 0, 0);
+                jthrowable jthrowable = mEnv->ExceptionOccurred();
+                if(jthrowable == nullptr)
+                    LOGE("异常为空", errorCode, error_cnt);
+//                mEnv->ThrowNew(mEnv->FindClass("java/lang/Exception"), "哦豁");
+            }
     }
 
     jvm->DetachCurrentThread();
@@ -99,7 +85,6 @@ Java_com_monree_crashcatch_JNI_test3(JNIEnv *env, jclass clazz) {
     jint rs = env->GetJavaVM(&jvm);
     assert(rs == JNI_OK);
 
-    mEnv = env;
     // 代码跳转锚点
     if (sigsetjmp(JUMP_ANCHOR, 1) != 0) {
 //        return -1;
@@ -115,7 +100,6 @@ Java_com_monree_crashcatch_JNI_test3(JNIEnv *env, jclass clazz) {
         sigemptyset(&block_mask);
         sigaddset(&block_mask, SIGABRT); // handler处理捕捉到的信号量时，需要阻塞的信号
         sigaddset(&block_mask, SIGSEGV); // handler处理捕捉到的信号量时，需要阻塞的信号
-        sigaddset(&block_mask, SIGSEGV);
 
         sigemptyset(&sigact.sa_mask);
         sigact.sa_flags = 0;
